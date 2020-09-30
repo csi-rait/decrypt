9863197414da2f6a4693d64a3a65c94d22232409
Fayyr <patushir@gmail.com>
2020-10-01 00:58:44 +0530

    deleted bl-content

--- bl-content/databases/plugins/about/db.php
+++
@@ -1,1 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
--- bl-content/databases/security.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{
-    "minutesBlocked": 5,
-    "numberFailuresAllowed": 10,
-    "blackList": []
-}

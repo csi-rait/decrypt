ea589c0b68ff552d2830102a2d194a4b8acbcc4e
Mukulika <mukulikapahari@gmail.com>
2020-10-14 23:01:21 +0530

    Feature: Post author metadata

--- bl-themes/sea-point-master/php/page.php
+++ bl-themes/sea-point-master/php/page.php
@@ -13,11 +13,33 @@
 			
 				<div class="col-lg-10">
 					<h2> <?php echo $page->title() ?></h2>
-					<h3 class="subheading hidden"><?php echo $page->description() ?></h3>
-						<!--<?php echo $language->get('Posted by').' '.$page->user('nickname').' - '.$page->date() ?></p>
-						<?php if (!$page->isStatic() && !$url->notFound()): ?>-->
+					<!--<h3 class="subheading hidden"><?php echo $page->description() ?></h3>
+						<?php echo $language->get('Posted by').' '.$page->user('nickname').' - '.$page->date() ?></p>
+						<?php if (!$page->isStatic() && !$url->notFound()): ?>
 						<span class="author-name"><h5>Posted By <a style="color:cyan;"href="#0"><?php echo $page->user('nickname') ?></a> </h5></span>
 						<p class="post-meta"><?php echo $page->date();?></p>
+						<?php endif ?>-->
+				<?php if (!$page->isStatic() && !$url->notFound()): ?>
+				<div class="d-flex ">
+					<div class="p-2"><img class="rounded-circle" src="bl-content\uploads\profiles\admin.png" width="40"></div>
+					<div class="p-2">
+						<span class="author-name" style="color:cyan;"><?php echo $page->user('nickname') ?></span><br>
+						<span class="post-meta"><?php echo $page->date();?></span>
+					</div>
+					<div class="ml-auto p-2">
+						<div class="social-buttons">
+							<?php if ($page->user('twitter')): ?>
+							<a href="<?php echo $page->user('twitter') ?>"><i class="fab fa-medium-m"></i></a>
+							<?php endif ?>
+							<?php if ($page->user('instagram')): ?>
+							<a href="<?php echo $page->user('instagram') ?>"><i class="fab fa-instagram"></i></a>
+							<?php endif ?>
+							<?php if ($page->user('linkedin')): ?>
+							<a href="<?php echo $page->user('linkedin') ?>"><i class="fab fa-linkedin-in"></i></a>
+							<?php endif ?>
+						</div>
+					</div>
+				</div>
 					<?php endif ?>
 				</div>
 			
